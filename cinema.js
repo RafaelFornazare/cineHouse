ac3ad70f10a96c003e446c27a49edc59866675f5
RafaelFornazare <rafael_fornazare@hotmail.com>
2022-10-24 20:02:45 -0300

    aula24/10/2022

--- cinema.js
+++ cinema.js
@@ -1,16 +1,15 @@
 const catalogo = require('./database/catalogo.json')
-//console.log (catalogo)
+
 // listar todos os filmes que estão em cartaz (listarTodosOsFilmesEmCartaz)
 
-const listarTodosOsFilmesEmCartaz = ()=>{
-    catalogo.forEach((filme)=> console.log (`Nome do filme : ${filme.titulo}`))
+const listarTodosOsFilmesEmCartaz = ( ) => {
+    catalogo . forEach ((filme) => console.log (`Nome do filme : ${filme.titulo}`))
 }
 
 // buscar um filme específico pelo código dele (buscarFilme) -> parametro: codigo
 
-const buscarFilme = (codigo) =>catalogo.find((filme)=> filme.codigo == codigo)
+const buscarFilme = (codigoParametro) =>catalogo.find((filme)=> filme . codigo == codigoParametro)
 
-//console.log(catalogo.filter((filme)=> filme.codigo === 2))
 // alterar o status de emCartaz (true -> false e se tiver false -> true) (alterarStatusEmCartaz) -> parametro: codigo
 
 const alterarStatusEmCartaz = (codigo, callback) =>{
@@ -19,11 +18,49 @@ const alterarStatusEmCartaz = (codigo, callback) =>{
         console.log("filme não encontrado!")
         return null;
     }
-    filme.emCartaz == true ? filme.emCartaz = false : filme.emCartaz = true ;
+    filme . emCartaz == true ? filme . emCartaz = false : filme . emCartaz = true ;
     return filme;
 }
 //let filme =catalogo.find((filme)=> filme.codigo ==codigo)
 //filme.emCartaz =! filme.emCartaz ´´´outro metodo 
 
-console.log(alterarStatusEmCartaz(5, buscarFilme))
-listarTodosOsFilmesEmCartaz()
+
+// adicionar um novo filme no nosso catálogo (adicionarFilme) ->
+// parametro: filme: { codigo, titulo, atores, duracao, anoDeLancamento }
+const  adicionarFilme  =  ( filme )  =>  {
+    const  { codigo , titulo , atores , duracao , anoDeLancamento } = filme ;  
+    if  ( ! duracao )  {
+      consola . log ( "Não é possível adicionar um filme sem duração" ) ;
+      Return 
+    }
+    const  filmeParaAdicionar  = {
+        ... filme ,
+        emCartaz : true
+    }
+    
+    catalogo . push ( filmeParaAdicionar ) ;
+    voltar  filmeParaAdicionar ;
+  }
+  // listar os filmes com duração maior do que 2h (listarFilmesComLongaDuracao) -> filter;
+  const  listarFilmesComLongaDuracao  =  ( )  =>  {
+    voltar  catálogo . filter ( ( filme )  =>  filme . duracao  >  2 )  // [{}, {}, {}]
+  }
+  // console.log(alterarStatusEmCartaz(1, buscarFilme))
+  // listarTodosOsFilmesEmCartaz()
+    adicionarFilme ( { 
+      código : 6 , 
+      Título : "O Poderoso Chefão" ,
+      atores : [ "Marlon Brando" ,  "Al Pacino" ,  "James Caan" ] ,
+      duração : 3,5 ,
+      anoDeLançamento : 1972
+    } )
+    // chamar a função de listar os filmes aqui depois de chamar a função de adicionar
+    consola . log ( listarFilmesComLongaDuracao ( ) )
+  
+    adicionarFilme ( {
+      código : 7 ,
+      título : "O Poderoso Chefão 2" ,
+      atores : [ "Marlon Brando" ,  "Al Pacino" ,  "James Caan" ] ,
+      anoDeLancamento : 1974 ,
+      duração : 2
+    } )
